a95e9add3d70039fcefad1f1b5123b11858a495b
sebzaliebdurrehimli <esebzali@gmail.com>
2022-02-01 01:38:08 +0400

    backend

---
+++ src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.backend.service;
+import com.example.backend.dto.UserCreateDTO;
+import com.example.backend.dto.UserUpdateDTO;
+import com.example.backend.dto.UserViewDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+
+public interface UserService {
+
+    UserViewDTO getUserById(Long id);
+
+    List<UserViewDTO> getUsers();
+
+    UserViewDTO createUser(UserCreateDTO userCreateDTO);
+
+
+    UserViewDTO updateUser(Long id, UserUpdateDTO userUpdateDTO);
+
+    void deleteUser(Long id);
+
+    List<UserViewDTO> slice(Pageable pageable);
+}
---
+++ src/main/java/com/example/backend/util/GenericResponse.java
@@ -0,0 +1,16 @@
+package com.example.backend.util;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public final class GenericResponse {
+
+    private String message;
+
+    }
+
